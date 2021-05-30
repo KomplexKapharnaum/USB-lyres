eb7901e7e47c1f43ddd60bc5e3e4def0ab487993
maigre <thomas@hmsphr.com>
2021-05-30 23:09:15 +0200

    KArtNet

---
+++ KArtNet/main.js
@@ -0,0 +1,40 @@
+
+//
+// Note: to make release: cd release; pkg ../
+//
+
+const VERSION = "1.0.0"
+
+// ARTNET
+//
+const UNIVERSE = 17
+
+var artnet = require('artnet')({
+    // host: '127.0.0.1',   // default: 255.255.255.255
+    refresh: 1000,          // default: 4000
+    // sendAll: true        // default: send only new values
+});
+
+// MIDI
+//
+var easymidi = require('easymidi');
+var input = new easymidi.Input('KArtNet-32', true);
+
+input.on('cc', function (data) {
+    // console.log(data)
+
+    if (data.controller > 32) return;
+
+    dmxChannel = data.controller + 32 * data.channel
+
+    artnet.set(UNIVERSE, dmxChannel, data.value*2, function (err, res) {
+        console.log(err, res)
+    });
+});
+
+console.log(".:: KArtNet-32 v"+VERSION+" ::.")
+console.log("Forward midi CC 1->32 to ArtNet universe "+UNIVERSE)
+console.log("DMX channel = CC n° + 32 * MIDI channel")
+console.log("DMX value = MIDI value * 2")
+console.log("ready :)")
+
