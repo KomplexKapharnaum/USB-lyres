19e8bfeaf349acc052bb1228fe805724f610e32b
maigre <thomas.bohl@gmail.com>
2021-05-31 10:32:06 +0200

    cleanup

--- KArtNet/main.js
+++ KArtNet/main.js
@@ -1,4 +1,3 @@
-
 //
 // Note: to make release: cd release; pkg ../
 //
@@ -7,12 +6,12 @@ const VERSION = "1.0.0"
 
 // ARTNET
 //
-const UNIVERSE = 17
+const UNIVERSE = 16
 
 var artnet = require('artnet')({
-    // host: '127.0.0.1',   // default: 255.255.255.255
-    refresh: 1000,          // default: 4000
-    // sendAll: true        // default: send only new values
+    host: '2.255.255.255', // default: 255.255.255.255
+    refresh: 1000, // default: 4000
+    sendAll: true // default: send only new values
 });
 
 // MIDI
@@ -20,21 +19,20 @@ var artnet = require('artnet')({
 var easymidi = require('easymidi');
 var input = new easymidi.Input('KArtNet-32', true);
 
-input.on('cc', function (data) {
+input.on('cc', function(data) {
     // console.log(data)
 
     if (data.controller > 32) return;
 
     dmxChannel = data.controller + 32 * data.channel
 
-    artnet.set(UNIVERSE, dmxChannel, data.value*2, function (err, res) {
+    artnet.set(UNIVERSE, dmxChannel, data.value * 2, function(err, res) {
         console.log(err, res)
     });
 });
 
-console.log(".:: KArtNet-32 v"+VERSION+" ::.")
-console.log("Forward midi CC 1->32 to ArtNet universe "+UNIVERSE)
+console.log(".:: KArtNet-32 v" + VERSION + " ::.")
+console.log("Forward midi CC 1->32 to ArtNet universe " + UNIVERSE)
 console.log("DMX channel = CC n° + 32 * MIDI channel")
 console.log("DMX value = MIDI value * 2")
 console.log("ready :)")
-
